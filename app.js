4ccbd320f4e03476bb42947421945043b162cf61
Daniil Larionov <red7172@yandex.ru>
2021-12-23 10:50:56 +0400

    fix: подключить cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const mongoose = require('mongoose');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const errorHandler = require('./middlewares/errorhandler');
@@ -12,6 +13,7 @@ const { NODE_ENV, MONGO_URL } = process.env;
 
 const PORT = 3000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb', {
